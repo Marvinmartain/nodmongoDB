52eca3c89622397064c028dfba38eef50693acec
Marvinmartain <122340360+Marvinmartain@users.noreply.github.com>
2023-04-19 11:35:31 -0400

    not working

--- routes/users.js
+++ routes/users.js
@@ -26,7 +26,31 @@ router.put("/:id", async (req, res) => {
   }
 });
 
-//de
+//delete user
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("Account has been deleted");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json("You can delete only your account!");
+  }
+});
+
+//get a user
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, updatedAt, ...other } = user._doc;
+    res.status(200).json(other);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 //follow a user
 
 router.put("/:id/follow", async (req, res) => {
